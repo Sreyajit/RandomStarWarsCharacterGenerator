116a421e8c06864968a0bb94a96ff3586217dcef
Sreyajit <sreyajit25@gmail.com>
2023-02-01 19:09:30 +0530

    SLow api endpoint but it works

---
+++ randstwchargen.js
@@ -0,0 +1,39 @@
+"use strict";
+let playButton = document.getElementById("playbutton");
+playButton.addEventListener("click", () => {
+	let name = document.getElementById("name");
+	let home = document.getElementById("Home");
+	let movies = document.getElementById("movies");
+	var people = {};
+	var homeWorld = {};
+	var films = {};
+	function getRndInteger(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	let random = getRndInteger(1, 83);
+	let randomURLPeople = "https://swapi.dev/api/people/" + random + "/";
+	fetch(randomURLPeople)
+		.then((Response) => Response.json())
+		.then((data) => {
+			people = data;
+			name.innerHTML = people.name;
+			fetch(people.homeWorld)
+				.then((Response) => Response.json())
+				.then((data) => {
+					homeWorld = data;
+					home.innerHTML = homeWorld.name;
+				});
+			const orderedFilms = document.getElementById("movies");
+			orderedFilms.innerHTML = "<ul id='filmlist'></ul>";
+			for (var i = 0; i < people.films.length; i++) {
+				fetch(people.films[i])
+					.then((Response) => Response.json())
+					.then((data) => {
+						films = data;
+						let filmList = document.createElement("li");
+						filmList.innerHTML = films.title;
+						document.getElementById("filmlist").appendChild(filmList);
+					});
+			}
+		});
+});
